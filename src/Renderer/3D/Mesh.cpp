630036c055a1e51afa286f64da6a285031592439
Eugene Lebedev <mr.legenius@gmail.com>
2023-03-07 05:15:23 +0300

    Add objLoader

--- src/Renderer/3D/Mesh.cpp
+++ src/Renderer/3D/Mesh.cpp
@@ -29,8 +29,8 @@ std::shared_ptr<Mesh> Mesh::CreatePlane(int resolution)
 				resolution * i + j + resolution + 1,
 				resolution * i + j + 1);
 
-			mesh->triangles.emplace_back(triangle1);
-			mesh->triangles.emplace_back(triangle2);
+			mesh->AddTriangle(triangle1);
+			mesh->AddTriangle(triangle2);
 		}
 	}
 
@@ -146,18 +146,18 @@ std::shared_ptr<Mesh> Mesh::CreateCube()
 	mesh->uvs.emplace_back(DirectX::SimpleMath::Vector2(1.0f, 1.0f));
 
 	//indices
-	mesh->triangles.emplace_back(DirectX::SimpleMath::Vector3(0, 1, 2));
-	mesh->triangles.emplace_back(DirectX::SimpleMath::Vector3(0, 2, 3));
-	mesh->triangles.emplace_back(DirectX::SimpleMath::Vector3(4, 5, 6));
-	mesh->triangles.emplace_back(DirectX::SimpleMath::Vector3(4, 6, 7));
-	mesh->triangles.emplace_back(DirectX::SimpleMath::Vector3(8, 9, 10));
-	mesh->triangles.emplace_back(DirectX::SimpleMath::Vector3(8, 10, 11));
-	mesh->triangles.emplace_back(DirectX::SimpleMath::Vector3(12, 13, 14));
-	mesh->triangles.emplace_back(DirectX::SimpleMath::Vector3(12, 14, 15));
-	mesh->triangles.emplace_back(DirectX::SimpleMath::Vector3(16, 17, 18));
-	mesh->triangles.emplace_back(DirectX::SimpleMath::Vector3(16, 18, 19));
-	mesh->triangles.emplace_back(DirectX::SimpleMath::Vector3(20, 21, 22));
-	mesh->triangles.emplace_back(DirectX::SimpleMath::Vector3(20, 22, 23));
+	mesh->AddTriangle(DirectX::SimpleMath::Vector3(0, 1, 2));
+	mesh->AddTriangle(DirectX::SimpleMath::Vector3(0, 2, 3));
+	mesh->AddTriangle(DirectX::SimpleMath::Vector3(4, 5, 6));
+	mesh->AddTriangle(DirectX::SimpleMath::Vector3(4, 6, 7));
+	mesh->AddTriangle(DirectX::SimpleMath::Vector3(8, 9, 10));
+	mesh->AddTriangle(DirectX::SimpleMath::Vector3(8, 10, 11));
+	mesh->AddTriangle(DirectX::SimpleMath::Vector3(12, 13, 14));
+	mesh->AddTriangle(DirectX::SimpleMath::Vector3(12, 14, 15));
+	mesh->AddTriangle(DirectX::SimpleMath::Vector3(16, 17, 18));
+	mesh->AddTriangle(DirectX::SimpleMath::Vector3(16, 18, 19));
+	mesh->AddTriangle(DirectX::SimpleMath::Vector3(20, 21, 22));
+	mesh->AddTriangle(DirectX::SimpleMath::Vector3(20, 22, 23));
 
 	return mesh;
 }
@@ -205,7 +205,7 @@ std::shared_ptr<Mesh> Mesh::CreateSphere(const uint32_t sliceCount)
 	for (int i = 1; i <= sliceCount; i++) 
 	{
 		auto triangle = DirectX::SimpleMath::Vector3(0, i + 1, i);
-		mesh->triangles.emplace_back(triangle);
+		mesh->AddTriangle(triangle);
 	}
 
 	auto baseIndex = 1;
@@ -221,8 +221,8 @@ std::shared_ptr<Mesh> Mesh::CreateSphere(const uint32_t sliceCount)
 				baseIndex + i * ringVertexCount + j + 1,
 				baseIndex + (i + 1) * ringVertexCount + j + 1);
 
-			mesh->triangles.emplace_back(triangle1);
-			mesh->triangles.emplace_back(triangle2);
+			mesh->AddTriangle(triangle1);
+			mesh->AddTriangle(triangle2);
 		}
 	}
 	const auto southPoleIndex = mesh->GetVerticesCount() - 1;
@@ -230,7 +230,7 @@ std::shared_ptr<Mesh> Mesh::CreateSphere(const uint32_t sliceCount)
 	for (int i = 0; i < sliceCount; i++) {
 
 		auto triangle = DirectX::SimpleMath::Vector3(southPoleIndex, baseIndex + i, baseIndex + i + 1);
-		mesh->triangles.emplace_back(triangle);
+		mesh->AddTriangle(triangle);
 	}
 
 	return mesh;
@@ -238,19 +238,10 @@ std::shared_ptr<Mesh> Mesh::CreateSphere(const uint32_t sliceCount)
 
 std::shared_ptr<D3D11IndexBuffer> Mesh::CreateIndexBuffer() const
 {
-	auto trianglesCount = GetTrianglesCount();
-	auto indicesCount = trianglesCount * 3;
+	auto indicesCount = GetIndicesCount();
 	const auto indices = new uint32_t[indicesCount];
-	int offset = 0;
-	for (uint32_t i = 0; i < trianglesCount; i++)
-	{
-		auto triangle = GetTriangle(i);
-		indices[offset + 0] = triangle.x;
-		indices[offset + 1] = triangle.y;
-		indices[offset + 2] = triangle.z;
-
-		offset += 3;
-	}
+	for (uint32_t i = 0; i < indicesCount; i++)
+		indices[i] = GetIndex(i);
 
 	auto indexBuffer = std::make_shared<D3D11IndexBuffer>(indices, indicesCount);
 	delete[] indices;
--- src/Renderer/3D/Mesh.h
+++ src/Renderer/3D/Mesh.h
@@ -10,7 +10,7 @@ class Mesh
 {
 public:
 	static std::shared_ptr<Mesh> CreatePlane(int resolution);
-	static std::shared_ptr<Mesh> CreateSphere(const uint32_t numberSlices);
+	static std::shared_ptr<Mesh> CreateSphere(const uint32_t sliceCount);
 	static std::shared_ptr<Mesh> CreateCube();
 
 	std::shared_ptr<D3D11IndexBuffer> CreateIndexBuffer() const;
@@ -21,23 +21,36 @@ public:
 	{
 		vertices.clear();
 		uvs.clear();
-		triangles.clear();
+		indices.clear();
 		normals.clear();
 	}
 
 	[[nodiscard]] uint32_t GetVerticesCount() const { return vertices.size(); }
 	[[nodiscard]] DirectX::SimpleMath::Vector3 GetVertex(uint32_t index) const { return vertices[index]; }
+	void AddVertex(const DirectX::SimpleMath::Vector3& value) { vertices.emplace_back(value); }
 
-	[[nodiscard]] uint32_t GetTrianglesCount() const { return triangles.size(); }
-	[[nodiscard]] DirectX::SimpleMath::Vector3 GetTriangle(uint32_t index) const { return triangles[index]; }
+	[[nodiscard]] uint32_t GetIndicesCount() const { return indices.size(); }
+	[[nodiscard]] uint32_t GetIndex(uint32_t index) const { return indices[index]; }
+	void AddTriangle(const DirectX::SimpleMath::Vector3& value)
+	{
+		indices.emplace_back(value.x);
+		indices.emplace_back(value.y);
+		indices.emplace_back(value.z);
+	}
+
+	void AddIndex(const uint32_t& value) { indices.emplace_back(value); }
 
 	[[nodiscard]] DirectX::SimpleMath::Vector2 GetUV(uint32_t index) const { return uvs[index]; }
+	void AddUV(const DirectX::SimpleMath::Vector2& value) { uvs.emplace_back(value); }
+
 	[[nodiscard]] DirectX::SimpleMath::Vector3 GetNormal(uint32_t index) const { return normals[index]; }
+	void AddNormal(const DirectX::SimpleMath::Vector3& value) { normals.emplace_back(value); }
 
 private:
 	std::vector<DirectX::SimpleMath::Vector3> vertices;
 	std::vector<DirectX::SimpleMath::Vector2> uvs;
-	std::vector<DirectX::SimpleMath::Vector3> triangles;
+	std::vector<uint32_t> indices;
 	std::vector<DirectX::SimpleMath::Vector3> normals;
+
 };
 
