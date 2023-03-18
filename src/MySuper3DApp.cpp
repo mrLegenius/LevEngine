b4052c46088be5148aa4b758165bc7523318d5a6
Eugene Lebedev <mr.legenius@gmail.com>
2023-03-18 08:35:00 +0300

    Add profiling sessions

--- src/MySuper3DApp.cpp
+++ src/MySuper3DApp.cpp
@@ -1,3 +1,4 @@
+#include "Debug/Profiler.h"
 #include "FirstSteps/MySuper3DLayer.h"
 #include "Pong/PongLayer.h"
 #include "SolarSystem/SolarSystemLayer.h"
@@ -6,11 +7,16 @@
 
 int main()
 {
+	LEV_PROFILE_BEGIN_SESSION("Startup", "LevEngineProfile-Startup.json");
+
 	Application app("My3DApp", 800, 800);
 	//app.PushLayer(new MySuper3DLayer);
 	//app.PushLayer(new PongLayer);
 	//app.PushLayer(new SolarSystemLayer);
 	app.PushLayer(new KatamariLayer);
+	LEV_PROFILE_END_SESSION();
 
+	LEV_PROFILE_BEGIN_SESSION("Runtime", "LevEngineProfile-Runtime.json");
 	app.Run();
+	LEV_PROFILE_END_SESSION();
 }
