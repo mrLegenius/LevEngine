eb27437d3ee3c4928b55b5fc207afcbae611fd8b
mr.legenius <mr.legenius@gmail.com>
2023-08-09 22:17:03 +0300

    Resize viewport texture with main render target

--- src/Katamari/Panels/ViewportPanel.h
+++ src/Katamari/Panels/ViewportPanel.h
@@ -10,12 +10,25 @@ namespace LevEngine::Editor
 		explicit ViewportPanel(const Ref<Texture>& renderTexture)
 		{
 			m_Texture = renderTexture->Clone();
+
+			m_Size.x = renderTexture->GetWidth();
+			m_Size.y = renderTexture->GetHeight();
 		}
 
 		void UpdateViewportTexture(const Ref<Texture>& renderTexture) const
 		{
+			const auto targetWidth = renderTexture->GetWidth();
+			const auto targetHeight = renderTexture->GetHeight();
+
+			if (targetWidth != m_Texture->GetWidth() || targetHeight != m_Texture->GetHeight())
+				m_Texture->Resize(targetWidth, targetHeight);
+
 			m_Texture->CopyFrom(renderTexture);
 		}
+
+		[[nodiscard]] float GetWidth() const { return m_Size.x; }
+		[[nodiscard]] float GetHeight() const { return m_Size.y; }
+
 	protected:
 		std::string GetName() override { return "Viewport"; }
 		void DrawContent() override;
