636d608ccce4f2e286d9ccfcf8b17afbe8603679
mr.legenius <mr.legenius@gmail.com>
2023-09-13 16:59:26 +0300

    Add GUI utils for runtime UI

---
+++ LevEngine/src/GUI/GUI.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "Kernel/Color.h"
+#include "Math/Vector2.h"
+
+namespace LevEngine
+{
+    class GUI
+    {
+    public:
+        static Vector2 GetWindowPosition();
+
+        static Vector2 GetWindowSize();
+
+        static void DrawCircle(Vector2 pos, float radius, Color color, float thickness = 1);
+
+        //TODO: Pass font here
+        static void DrawString(Vector2 pos, const String& text, float size, Color color);
+    };
+}
