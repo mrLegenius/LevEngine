e7d2e68b7f4d5af32600a72138f8c575455132ce
Eugene Lebedev <mr.legenius@gmail.com>
2023-09-15 20:21:14 +0300

    Add modal popup

---
+++ LevEditor/src/ModalPopup.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+
+namespace LevEngine::Editor
+{
+class ModalPopup
+{
+public:
+    static void Render() { return Get().RenderPopup(); }
+
+    static void Show(
+        String title,
+        String description,
+        String buttonText,
+        const std::function<void()>& buttonAction);
+
+    static void Show(
+        String title,
+        String description,
+        String leftButtonText,
+        String rightButtonText,
+        const std::function<void()>& leftButtonAction = nullptr,
+        const std::function<void()>& rightButtonAction = nullptr);
+
+private:
+
+    void RenderPopup() const;
+
+    static ModalPopup& Get()
+    {
+        static ModalPopup popup;
+        return popup;
+    }
+
+    inline static bool m_PopupRequested;
+    
+    String m_Title;
+    String m_Description;
+    String m_LeftButtonText;
+    String m_RightButtonText;
+
+    std::function<void()> m_LeftButtonAction;
+    std::function<void()> m_RightButtonAction;
+};
+}
