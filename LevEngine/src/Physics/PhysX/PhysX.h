d69f232dc88fa53f6c3d9d533b0e0eadff1f52fa
Nemkov Yaroslav <nemkofff.slava@gmail.com>
2023-10-05 16:05:41 +0300

    PhysX GPU Bug Commit

---
+++ LevEngine/src/Physics/PhysX/PhysX.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include "physx/include/PxPhysicsAPI.h"
+
+namespace LevEngine
+{
+    class PhysX
+    {
+    public:
+        PhysX();
+        ~PhysX();
+    
+    private:
+        physx::PxDefaultAllocator      gAllocator;         // via this interface PhysX performs all allocations (need to initialize PhysX)
+        physx::PxDefaultErrorCallback  gErrorCallback;     // via this interface PhysX logs all error messages (need to initialize PhysX)
+        physx::PxTolerancesScale       gToleranceScale;    
+        physx::PxFoundation*           gFoundation = NULL;
+        physx::PxPhysics*              gPhysics    = NULL;
+        physx::PxDefaultCpuDispatcher* gDispatcher = NULL;
+        physx::PxScene*                gScene      = NULL;
+        physx::PxMaterial*             gMaterial   = NULL;
+        physx::PxPvd*                  gPvd        = NULL;
+    };
+}
