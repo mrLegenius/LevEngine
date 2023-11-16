82add3da9bf5ba57881a6850a11a7edeb38731d6
Yaroslav Nemkov <nemkofff.slava@gmail.com>
2023-11-16 22:40:07 +0300

    [Physics] Rebase on DEV branch
    There is a problem with physics init

---
+++ LevEngine/src/Scene/Systems/Physics/PhysicsRigidbodyInitSystem.cpp
@@ -0,0 +1,20 @@
+﻿#include "levpch.h"
+#include "PhysicsInitSystem.h"
+
+#include "Physics/PhysX/PhysicsRigidbody.h"
+
+void LevEngine::PhysicsRigidbodyInitSystem::Update(float deltaTime, entt::registry& registry)
+{
+    const auto view = registry.view<PhysicsRigidbody>();
+
+    for (const auto entity : view)
+    {
+        PhysicsRigidbody& physicsRigidbodyComponent = view.get<PhysicsRigidbody>(entity);
+
+        if (!physicsRigidbodyComponent.IsInitialized())
+        {
+            auto entityWrapped = Entity(entt::handle{ registry, entity });
+            physicsRigidbodyComponent.Init(entityWrapped);
+        }
+    }
+}
