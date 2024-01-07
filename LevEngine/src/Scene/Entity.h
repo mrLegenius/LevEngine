13fb20c6913e06762081f1ae877ed4ee93ffe8f0
Eugene Lebedev <mr.legenius@gmail.com>
2024-01-07 11:06:30 +0300

    Fix hard lock

--- LevEngine/src/Scene/Entity.h
+++ LevEngine/src/Scene/Entity.h
@@ -53,7 +53,8 @@ namespace LevEngine
     private:
         entt::handle m_Handle;
 
-        static inline std::mutex s_Mutex;
+        template <typename T>
+        inline static std::recursive_mutex s_Mutex;
     };
 }
 
