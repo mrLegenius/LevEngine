c29a6c85d432a1bbca7992a74d7f71806b0a3309
Fenrir7Asteron <b.fedotov@everplay.dev>
2024-01-16 10:30:58 +0300

    Move animation loading to separate method

--- LevEngine/src/Assets/MeshAsset.cpp
+++ LevEngine/src/Assets/MeshAsset.cpp
@@ -19,37 +19,50 @@ namespace LevEngine
         try
         {
             m_Mesh = MeshLoader::LoadMesh(m_Path);
-            const auto animations = AnimationLoader::LoadAllAnimations(m_Path, m_Mesh);
-            for (int animationIdx = 0; animationIdx < animations.size(); ++animationIdx)
-            {
-                String animationName = animations[animationIdx]->GetName();
+        }
+        catch (std::exception& e)
+        {
+            Log::CoreWarning("Failed to load mesh in {0}. Error: {1}", m_Path.string(), e.what());
+        }
 
-                const size_t idx = animationName.find_last_of("|");
-                if (idx != String::npos)
-                {
-                    animationName = animationName.substr(idx + 1);
-                }
-                
-                String nameToWrite = (animationName.empty()
-                    ? ToString(animationIdx)
-                      : animationName)
-                + String(".anim");
-                
-                const Path animationAssetPath = m_Path.parent_path().append(nameToWrite.c_str());
-                
-                if (!AssetDatabase::AssetExists(animationAssetPath))
-                {
-                    const Ref<AnimationAsset> animationAsset =
-                        AssetDatabase::CreateNewAsset<AnimationAsset>(animationAssetPath);
-
-                    animationAsset->Init(animations[animationIdx], animationIdx, shared_from_this());
-                    animationAsset->Serialize();
-                }
-            }
+        try
+        {
+            LoadAnimations();
         }
         catch (std::exception& e)
         {
             Log::CoreWarning("Failed to load animations in {0}. Error: {1}", m_Path.string(), e.what());
+        }
+    }
+
+    void MeshAsset::LoadAnimations()
+    {
+        const auto animations = AnimationLoader::LoadAllAnimations(m_Path, m_Mesh);
+        for (int animationIdx = 0; animationIdx < animations.size(); ++animationIdx)
+        {
+            String animationName = animations[animationIdx]->GetName();
+
+            const size_t idx = animationName.find_last_of("|");
+            if (idx != String::npos)
+            {
+                animationName = animationName.substr(idx + 1);
+            }
+                
+            String nameToWrite = (animationName.empty()
+                                      ? ToString(animationIdx)
+                                      : animationName)
+                + String(".anim");
+                
+            const Path animationAssetPath = m_Path.parent_path().append(nameToWrite.c_str());
+                
+            if (!AssetDatabase::AssetExists(animationAssetPath))
+            {
+                const Ref<AnimationAsset> animationAsset =
+                    AssetDatabase::CreateNewAsset<AnimationAsset>(animationAssetPath);
+
+                animationAsset->Init(animations[animationIdx], animationIdx, shared_from_this());
+                animationAsset->Serialize();
+            }
         }
     }
 }
