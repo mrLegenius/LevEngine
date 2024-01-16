b534c380f12f08edccea9007eda6306d2dc79900
Eugene Lebedev <mr.legenius@gmail.com>
2024-01-16 17:44:22 +0300

    Fix menu, tool and status bars

--- LevEditor/src/Panels/StatusBar.cpp
+++ LevEditor/src/Panels/StatusBar.cpp
@@ -10,33 +10,24 @@ namespace LevEngine::Editor
         m_Height = 10;
     }
 
-    void StatusBar::Render() const
+    void StatusBar::Render()
     {
-        const ImGuiViewport* viewport = ImGui::GetMainViewport();
-        ImGui::SetNextWindowPos(ImVec2(viewport->Pos.x, m_Height));
-        ImGui::SetNextWindowSize(ImVec2(viewport->Size.x, m_Height));
-        ImGui::SetNextWindowViewport(viewport->ID);
+        constexpr ImGuiWindowFlags windowFlags =
+            ImGuiWindowFlags_NoScrollbar
+            | ImGuiWindowFlags_NoSavedSettings
+            | ImGuiWindowFlags_MenuBar;
 
-        GUI::ScopedVariable windowPadding(ImGuiStyleVar_WindowPadding, Vector2(0, 2));
-        GUI::ScopedVariable itemInnerSpacing(ImGuiStyleVar_ItemInnerSpacing, Vector2::Zero);
-        GUI::ScopedVariable windowBorderSize(ImGuiStyleVar_WindowBorderSize, 0);
-        
-        GUI::ScopedColor buttonColor(ImGuiCol_Button, Color::Clear);
-        const auto& colors = ImGui::GetStyle().Colors;
-        const auto& buttonHovered = colors[ImGuiCol_ButtonHovered];
-        GUI::ScopedColor buttonHoveredColor(ImGuiCol_ButtonHovered, Color(buttonHovered.x, buttonHovered.y, buttonHovered.z, 0.5f));
-        const auto& buttonActive = colors[ImGuiCol_ButtonActive];
-        GUI::ScopedColor buttonActiveColor(ImGuiCol_ButtonActive, Color(buttonActive.x, buttonActive.y, buttonActive.z, 0.5f));
-        
-        constexpr ImGuiWindowFlags windowFlags = 0
-            | ImGuiWindowFlags_NoDocking
-            | ImGuiWindowFlags_NoTitleBar
-            | ImGuiWindowFlags_NoResize
-            | ImGuiWindowFlags_NoMove
-            | ImGuiWindowFlags_NoScrollbar
-            | ImGuiWindowFlags_NoSavedSettings;
+        m_Height = ImGui::GetFrameHeight();
 
-        ImGui::Begin("##statusbar", nullptr, windowFlags);
+        if (ImGui::BeginViewportSideBar("##MainStatusBar", nullptr, ImGuiDir_Down, m_Height, windowFlags))
+        {
+            if (ImGui::BeginMenuBar())
+            {
+                ImGui::Text("v0.0.1");
+                ImGui::EndMenuBar();
+            }
+        }
+
         ImGui::End();
     }
 }
